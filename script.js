295403658cb4f98cdbe21a8efdd3d2dc0bb2c50a
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2019-11-26 17:05:18 +0100

    send data to database

--- script.js
+++ script.js
@@ -1,99 +1,129 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-	const animationDelay = 1;
-	const removeLastTask = 1900;
-	const clearList = 4000;
+			const animationDelay = 1;
+			const removeLastTask = 1900;
+			const clearList = 4000;
 
-	setTimeout(() => {
-		let liAppear = document.querySelectorAll('.appear');
-		for (let i = 0; i < liAppear.length; i++) {
-			liAppear[i].classList.remove('appear');
-		}
-	}, animationDelay);
-
-	const btnAdd = document.querySelector('.btn.add');
-	const btnDel = document.querySelector('.btn.delete');
-	const btnCl = document.querySelector('.btn.clear');
-	let counter = 1;
-	const ul = document.querySelector('ul');
-	let task = document.querySelector('input');
-
-	const arrayTasks = localStorage.getItem('taskList') ?
-		JSON.parse(localStorage.getItem('taskList')) : [];
-
-	localStorage.setItem('taskList', JSON.stringify(arrayTasks))
-	let arrayTasks2 = JSON.parse(localStorage.getItem('taskList'))
-
-	let liMaker = (text) => {
-		var li = document.createElement('li');
-		li.textContent = text + ' -#' + counter++;
-		li.classList.add('appear')
-		ul.appendChild(li);
-	};
-
-	/*
-	 * add element
-	 */
-	btnAdd.addEventListener('click', () => {
-		if (task.value.trim() === '') {
-			let emptyTxt = task.value.replace(/\s/g, '')
-			task.value = emptyTxt;
-			task.placeholder = 'write a task';
-			return
-		} else {
-			task.placeholder = 'to do ...';
-		}
-
-		let taskValue = task.value
-
-		arrayTasks.push(taskValue);
-		localStorage.setItem('taskList', JSON.stringify(arrayTasks));
-		liMaker(taskValue);
-		task.value = '';
-	});
-
-	arrayTasks2.forEach((e) => {
-		liMaker(e)
-	});
-
-	/*
-	 * remove last element
-	 */
-	btnDel.addEventListener('click', () => {
-		let lastTask = document.querySelector('ul li:last-child');
-
-		if (lastTask) {
-			lastTask.classList.add('removeLi');
 			setTimeout(() => {
-				lastTask.parentElement.removeChild(lastTask);
-			}, removeLastTask);
-			counter--
-		}
+				let liAppear = document.querySelectorAll('.appear');
+				for (let i = 0; i < liAppear.length; i++) {
+					liAppear[i].classList.remove('appear');
+				}
+			}, animationDelay);
 
-		arrayTasks.pop();
-		localStorage.setItem('taskList', JSON.stringify(arrayTasks));
-	});
-
-	/*
-	 * clear list
-	 */
-	btnCl.addEventListener('click', () => {
-		localStorage.clear();
-		let taskList = document.querySelectorAll('ul li')
-
-		for (let i = 0; i < taskList.length; i++) {
-			if (i % 2 == 0) {
-				taskList[i].classList.add('leftDirection')
-			} else {
-				taskList[i].classList.add('rightDirection')
+			const btnAdd = document.querySelector('.btn.add');
+			const btnDel = document.querySelector('.btn.delete');
+			const btnCl = document.querySelector('.btn.clear');
+			let counter = 1;
+			const arrayId = [0];
+			const sortNr = (a, b) => {
+				return a - b;
 			}
-			setTimeout(() => {
-				taskList[i].parentNode.removeChild(taskList[i]);
-			}, clearList);
-		}
+			const idTask = arrayId.sort(sortNr)[a.length - 1] + 1;
+			const ul = document.querySelector('ul');
+			let task = document.querySelector('input');
 
-		counter = 1;
-		arrayTasks = [];
-	});
+			let liMaker = (text) => {
+				var li = document.createElement('li');
+				li.textContent = text + ' -#' + counter++;
+				li.classList.add('appear');
+				li.setAttribute("data-id", )
+				ul.appendChild(li);
+			};
 
-});
+			/*
+			 * read element
+			 */
+			const readTask = () => {
+				clearTasksList();
+				$.ajax({
+					url: 'http://localhost:3000/tasks',
+					type: 'GET'
+				}).done(function (r) {
+					console.log(r);
+					r.forEach(function (e) {
+						console.log(e.id);
+						liMaker(e.task);
+						arrayId.push(e.id);
+
+					})
+				})
+			};
+			readTask();
+
+			/*
+			 * create element
+			 */
+			btnAdd.addEventListener('click', () => {
+				if (task.value.trim() === '') {
+					let emptyTxt = task.value.replace(/\s/g, '')
+					task.value = emptyTxt;
+					task.placeholder = 'write a task';
+					return
+				} else {
+					task.placeholder = 'to do ...';
+				}
+
+				let taskValue = task.value
+				$.ajax({
+					url: 'http://localhost:3000/tasks',
+					type: 'POST',
+					data: {
+						task: taskValue,
+						id: idTask
+					}
+				}).done(function (e) {
+					readTask();
+				})
+				task.value = '';
+			});
+
+
+			/*
+			 * remove last element
+			 */
+			btnDel.addEventListener('click', () => {
+				let lastTask = document.querySelector('ul li:last-child');
+
+				if (lastTask) {
+					let idTask = lastTask.getAttribute('data-id')
+					lastTask.classList.add('removeLi');
+					setTimeout(() => {
+						lastTask.parentElement.removeChild(lastTask);
+					}, removeLastTask);
+					counter--
+					deleteTask(idTask);
+				}
+			});
+
+			/*
+			 * clear list
+			 */
+			btnCl.addEventListener('click', clearTasksList);
+
+			const clearTasksList = () => {
+				let taskList = document.querySelectorAll('ul li');
+
+				for (let i = 0; i < taskList.length; i++) {
+					if (i % 2 == 0) {
+						taskList[i].classList.add('leftDirection');
+					} else {
+						taskList[i].classList.add('rightDirection');
+					}
+					setTimeout(() => {
+						taskList[i].parentNode.removeChild(taskList[i]);
+					}, clearList);
+					deleteTask(taskList[i].getAttribute('task-id'));
+				}
+
+				counter = 1;
+			}
+
+			const deleteTask = (id) => {
+				$.ajax({
+					url: 'http://localhost:3000/movies/' + id,
+					type: 'DELETE'
+				}).done(function () {
+					console.log('SUKCES USUNIET ELEMENT O ID =   ', id);
+				});
+			}
