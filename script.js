49c07071657468f7c156f9e3762196a7abfd32f7
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2019-11-28 12:32:05 +0100

    first change to jsonservr data

--- script.js
+++ script.js
@@ -1,87 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-	const animationDelay = 1;
-	const removeLastTask = 1900;
-	const clearList = 4000;
-
-	setTimeout(() => {
-		let liAppear = document.querySelectorAll('.appear');
-		for (let i = 0; i < liAppear.length; i++) {
-			liAppear[i].classList.remove('appear');
-		}
-	}, animationDelay);
-
+	let ulList = $('ul');
 	const btnAdd = document.querySelector('.btn.add');
 	const btnDel = document.querySelector('.btn.delete');
 	const btnCl = document.querySelector('.btn.clear');
 	let counter = 1;
-	const arrayId = [0];
-	const sortNr = (a, b) => {
-		return a - b;
-	}
-	const idTask = arrayId.sort(sortNr)[arrayId.length - 1] + 1;
+
 	const ul = document.querySelector('ul');
 	let task = document.querySelector('input');
 
 	let liMaker = (text) => {
 		var li = document.createElement('li');
-		li.textContent = text + ' -#' + counter++;
-		li.classList.add('appear');
-		li.setAttribute("data-id", idTask)
+		console.log("watość teskt to ", text.task);
+		li.textContent = text.task + ' -#' + counter++;
+		//li.classList.add('appear');
+		li.setAttribute("data-id", text.id)
 		ul.appendChild(li);
 	};
 
-	const clearTasksList = () => {
-		let taskList = document.querySelectorAll('ul li');
 
-		for (let i = 0; i < taskList.length; i++) {
-			if (i % 2 == 0) {
-				taskList[i].classList.add('leftDirection');
-			} else {
-				taskList[i].classList.add('rightDirection');
-			}
-			setTimeout(() => {
-				taskList[i].parentNode.removeChild(taskList[i]);
-			}, clearList);
-			deleteTask(taskList[i].getAttribute('task-id'));
-		}
+	/*read */
 
-		counter = 1;
-	}
-
-	const deleteTask = (id) => {
-		$.ajax({
-			url: 'http://localhost:3000/movies/' + id,
-			type: 'DELETE'
-		}).done(() => {
-			console.log('SUKCES USUNIET ELEMENT O ID =   ', id);
-		})
-	};
-
-	/*
-	 * read element
-	 */
-	const readTask = () => {
-		clearTasksList();
+	let readTask = () => {
+		ulList.empty();
 		$.ajax({
 			url: "http://localhost:3000/tasks",
 			type: "GET"
 		}).done(function (r) {
 			console.log(r);
 			r.forEach(function (e) {
-
-				console.log(e.id);
-				liMaker(e.task);
-				arrayId.push(e.id);
-
+				liMaker(e)
 			})
 		})
 	};
 	readTask();
 
-	/*
-	 * create element
-	 */
+
+	/*create*/
+
+	let createTask = (data) => {
+		$.ajax({
+			url: 'http://localhost:3000/tasks',
+			type: 'POST',
+			data: data
+		}).done(() => {
+			readTask();
+		})
+	}
+
+	/*delete*/
+
+	let deleteTask = (id) => {
+		$.ajax({
+			url: 'http://localhost:3000/tasks/' + id,
+			type: 'DELETE'
+		}).done(() => {
+			readTask();
+		})
+	};
+
+	/*add action*/
+
 	btnAdd.addEventListener('click', () => {
 		if (task.value.trim() === '') {
 			let emptyTxt = task.value.replace(/\s/g, '')
@@ -94,41 +72,37 @@ document.addEventListener("DOMContentLoaded", function () {
 		let taskValue = task.value;
 		let data = {
 			task: taskValue,
-			id: idTask
 		}
-
-		$.ajax({
-			url: 'http://localhost:3000/tasks',
-			type: 'POST',
-			data: JSON.stringify(data)
-		}).done(function (e) {
-			readTask();
-		})
+		createTask(data)
 		task.value = '';
 	});
 
 
-	/*
-	 * remove last element
-	 */
+	/*remove action*/
 	btnDel.addEventListener('click', () => {
 		let lastTask = document.querySelector('ul li:last-child');
 
 		if (lastTask) {
 			let idTask = lastTask.getAttribute('data-id')
 			lastTask.classList.add('removeLi');
-			setTimeout(() => {
-				lastTask.parentElement.removeChild(lastTask);
-			}, removeLastTask);
+			lastTask.parentElement.removeChild(lastTask);
+
 			counter--
+			console.log("idTask w funkcj usun osttn element to", idTask);
 			deleteTask(idTask);
 		}
 	});
 
-	/*
-	 * clear list
-	 */
-	btnCl.addEventListener('click', clearTasksList);
+	/*clear acton*/
+	btnCl.addEventListener('click', () => {
+		let tasksList = document.querySelectorAll('ul li');
+		for (let i = 0; i < tasksList.length; i++) {
+			let idTask = tasksList[i].getAttribute('data-id');
+			deleteTask(idTask);
+
+		}
+
+	});
 
 
 })
